b7473294f4b83f01f88b7d3e0aa5af2e8db73cb8
Jiso Chacko <jisochacko007@gmail.com>
2022-02-17 14:31:25 +0530

    final commit

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Banner from './components/Banner/Banner';
 import Navbar from './components/Navbar/Navbar';
 import RowPost from './components/RowPost/RowPost';
+import {originals, action} from './urls'
 
 function App() {
   return (
     <div className="App">
     <Navbar />
     <Banner />
-    <RowPost />
+    <RowPost url={originals} title='Netflix originals'/>
+    <RowPost url={action} title='Action' isSmall={true}/> 
     </div>
   );
 }
---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '71583dd52abb80e8e45d8358f23413b6'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
