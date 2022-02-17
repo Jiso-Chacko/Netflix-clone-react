dbfd4b6614d7592703ef94d240bea9fe6010c870
Jiso Chacko <jisochacko007@gmail.com>
2022-02-17 10:08:39 +0530

    first commit

---
+++ netflix/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner from './components/Banner/Banner';
+import Navbar from './components/Navbar/Navbar';
+import RowPost from './components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <Banner />
+    <RowPost />
+    </div>
+  );
+}
+
+export default App;
